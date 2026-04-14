e60244c7e4bf1c9c86b29264f64a5b8edfc7d47e
ywt <1625208758@qq.com>
2026-04-14 16:10:28 +0800

    完成了SAA05

--- SAA-05Prompt/src/main/java/org/example/controller/PromptController.java
+++ SAA-05Prompt/src/main/java/org/example/controller/PromptController.java
@@ -2,7 +2,9 @@ package org.example.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.ToolResponseMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 public class PromptController {
@@ -73,13 +77,66 @@ public class PromptController {
                 .content();
 
     }
-    @GetMapping("prompt/chat4")
-    public Flux<String> chat4(@RequestParam(name = "question",defaultValue = "你是谁") String question)
+    @GetMapping("/prompt/chat4")
+    public Flux<String> chat4(String question)
     {
-        return qwenChatClient.prompt()
-                .system("你是一个善于讲故事的助手,我向你提供一些关键词,你来进行故事的生成并且回复，字数控制在500字以内,回复格式:html")
-                .user( question)
-                .stream()
-                .content();
+        // 系统消息
+        SystemMessage systemMessage = new SystemMessage("你是一个讲故事的助手," +
+                "每个故事控制在600字以内且以HTML格式返回");
+
+        // 用户消息
+        UserMessage userMessage = new UserMessage(question);
+
+        Prompt prompt = new Prompt(userMessage, systemMessage);
+
+        return deepseekChatModel.stream( prompt)
+                .map(response -> response.getResults().get(0).getOutput().getText());
+
+    }
+
+    @GetMapping("/prompt/chat5")
+    public String chat5(String question)
+    {
+//        AssistantMessage assistantMessage = deepseekChatClient.prompt()
+//                .user(question)
+//                .call()
+//                .chatResponse()
+//                .getResult()
+//                .getOutput();
+//
+//        return assistantMessage.getText();
+
+        AssistantMessage assistantMessage = deepseekChatClient.prompt()
+                .user(question)
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput();
+
+
+        return assistantMessage.getText();
+    }
+
+    @GetMapping("/prompt/chat6")
+    public String chat6(String city)
+    {
+
+        String answer = deepseekChatClient.prompt()
+                .user(city + "未来3天天气情况如何?")
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getText();
+
+        ToolResponseMessage toolResponseMessage =
+                new ToolResponseMessage(
+                        List.of(new ToolResponseMessage.ToolResponse("1","获得天气",city + "的天气是晴天，温度25℃")
+                        )
+                );
+
+        String toolResponse = toolResponseMessage.getText();
+
+        return answer + toolResponse;
     }
 }
