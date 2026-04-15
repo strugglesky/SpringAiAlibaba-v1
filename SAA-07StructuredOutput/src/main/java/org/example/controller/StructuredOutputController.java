00aa6089ef70d00965b9a851a1e56d6a9a114749
Ywt <1625208758@qq.com>
2026-04-15 11:46:25 +0800

    完成SAA-07

--- SAA-07StructuredOutput/src/main/java/org/example/controller/StructuredOutputController.java
+++ SAA-07StructuredOutput/src/main/java/org/example/controller/StructuredOutputController.java
@@ -3,10 +3,13 @@ package org.example.controller;
 import jakarta.annotation.Resource;
 import org.example.records.StudentRecord;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 import java.util.function.Consumer;
 
 @RestController
@@ -18,17 +21,21 @@ public class StructuredOutputController {
     public StudentRecord chat(@RequestParam(name = "sname") String sname,
                               @RequestParam(name = "email") String email) {
 
-        return qwenChatClient.prompt().user(new Consumer<ChatClient.PromptUserSpec>()
-        {
-            @Override
-            public void accept(ChatClient.PromptUserSpec promptUserSpec)
-            {
-                promptUserSpec.text("学号1001，我叫{sname},大学专业计算机科学与技术,邮箱{email}")
-                        .param("sname",sname)
-                        .param("email",email);
-            }
-        }).call().entity(StudentRecord.class);
+        return qwenChatClient.prompt().user(promptUserSpec ->
+                promptUserSpec.text("学生姓名为" + sname + "，学号1001，邮箱为" + email + "专业为计算机科学与技术")
+                        .param("sname", sname).param("email", email))
+                .call()
+                .entity(StudentRecord.class);
 
+    }
+
+    @GetMapping("/structuredoutput/chat2")
+    public StudentRecord chat2(@RequestParam(name = "sname") String sname,
+                              @RequestParam(name = "email") String email) {
+        PromptTemplate promptTemplate = new PromptTemplate("学生姓名为{sname}，学号1002，邮箱为{email}专业为计算机科学与技术");
+        Prompt prompt = promptTemplate.create(Map.of("sname", sname, "email", email));
+
+        return qwenChatClient.prompt(prompt).call().entity(StudentRecord.class);
     }
 
 }
