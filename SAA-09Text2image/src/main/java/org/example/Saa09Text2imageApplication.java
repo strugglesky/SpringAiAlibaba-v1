27b37e416cd874794b59740683374e4dafb90b52
ywt <1625208758@qq.com>
2026-04-16 15:03:47 +0800

    创建了了SAA09

---
+++ SAA-09Text2image/src/main/java/org/example/Saa09Text2imageApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saa09Text2imageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Saa09Text2imageApplication.class, args);
+    }
+}
