8e47ac4cfbc17bd4acc7adb8aa22f85bde004ec3
Ywt <1625208758@qq.com>
2026-04-16 13:41:11 +0800

    完成SAA-08

---
+++ SAA-08Persistent/src/main/java/org/example/controller/ChatMemory4RedisController.java
@@ -0,0 +1,62 @@
+package org.example.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Consumer;
+
+/**
+ * @auther zzyybs@126.com
+ * @create 2025-07-28 18:40
+ * @Description TODO
+ */
+@RestController
+public class ChatMemory4RedisController
+{
+    @Resource(name = "qwenChatClient")
+    private ChatClient qwenChatClient;
+    @Autowired
+    private ChatModel qwen;
+
+    /**
+     * zzyybs@126.com
+     * @param msg
+     * @param userId
+     * @return
+     */
+    @GetMapping("/chatmemory/chat")
+    public String chat(String msg, String userId)
+    {
+        /*return qwenChatClient.prompt(msg).advisors(new Consumer<ChatClient.AdvisorSpec>()
+        {
+            @Override
+            public void accept(ChatClient.AdvisorSpec advisorSpec)
+            {
+                advisorSpec.param(CONVERSATION_ID, userId);
+            }
+        }).call().content();*/
+
+
+//        return qwenChatClient
+//                .prompt(msg)
+//                .advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, userId))
+//                .call()
+//                .content();
+
+        return qwenChatClient.prompt(msg)
+                .advisors(new Consumer<ChatClient.AdvisorSpec>() {
+                    @Override
+                    public void accept(ChatClient.AdvisorSpec advisorSpec) {
+                        advisorSpec.param(CONVERSATION_ID, "conv:user:" + userId);
+                    }
+                })
+                .call()
+                .content();
+
+    }
+}
