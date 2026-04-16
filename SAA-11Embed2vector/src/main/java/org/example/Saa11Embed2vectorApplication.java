3ab8e81ebd28b3732b2add443ce0a132f35bbc31
ywt <1625208758@qq.com>
2026-04-16 16:31:58 +0800

    创建SAA11

---
+++ SAA-11Embed2vector/src/main/java/org/example/Saa11Embed2vectorApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saa11Embed2vectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Saa11Embed2vectorApplication.class, args);
+    }
+}
